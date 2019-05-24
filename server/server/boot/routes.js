7b8ecc2429db0485fbd3f110a06ad0d92e8ca0f7
YanBt <yannick@botamp.com>
2019-05-24 23:59:15 +0000

    Added route to join conference in loopback

--- server/server/boot/routes.js
+++ server/server/boot/routes.js
@@ -1,34 +1,38 @@
-// module.exports = function (app) {
-//   const bodyParser = require('body-parser')
-//   app.use(bodyParser.json())
-//   app.post('/api/login', function(req, res) {
-//     console.log(">>>>>>>>>>>>REQ>>>>>>>>>")
-//     console.log(req)
-//     console.log(req.body)
-//     app.models.User.login({
-//       email: req.body.email,
-//       password: req.body.password
-//     }, 'user', function(err, token) {
-//       if (err) {
-//         res.render('response', { //render view named 'response.ejs'
-//           title: 'Login failed',
-//           content: err,
-//           redirectTo: '/',
-//           redirectToLinkText: 'Try again'
-//         });
-//         return;
-//       }
-
-//       res.json({
-//         email: req.body.email,
-//         accessToken: token.id
-//       });
-//     });
-//   });
-// }
-
-// module.exports = function (app) {
-//   app.get('/users/:id/conferences', function(req, res) {
-    
-//   });
-// }
+module.exports = function (app) {
+  app.get('/api/conferences/join', function(req, res) {
+    app.models.Speaker.findOne(
+    {
+      where: {email: req.query.email},
+      include: {
+        relation: 'conferences',
+        scope: {
+          where:{
+            codeAcces: req.query.codeacces
+          },
+          include: ['enquetes', 'questionnaires', 'questions']
+        }
+      }
+    }, function(err, speaker) {
+        let speakerJson = speaker.toJSON()
+        if (speaker == null) {
+          res.status(404)
+          res.json({
+            status: 404,
+            error: "No speaker found with the given email"
+          })
+        } else {
+          if(speakerJson.conferences.length == 0) {
+            res.status(404)
+            res.json({
+              status: 404,
+              error: "Wrong access code"
+            })
+          } else {
+            res.json (
+              speakerJson.conferences[0]
+            )
+          }
+        }
+    })
+  });
+}
